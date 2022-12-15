b9ffafdc1278e30032fa7bb8ca855d66b64ff37e
Samuel Martins <118900551+SamuelGSa@users.noreply.github.com>
2022-12-15 12:04:47 -0300

    Add files via upload

---
+++ src/repeticaoJava/ex003.java
@@ -0,0 +1,10 @@
+package repeticaoJava;
+
+public class ex003 {
+    //3- Solicitar a idade de várias pessoas e imprimir: Total de pessoas com menos de
+    //21 anos. Total de pessoas com mais de 50 anos. O programa termina quando
+    //idade for =-99. (WHILE)
+    public static void main(String[] args) {
+
+    }
+}
